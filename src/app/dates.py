c5cbeccbd1a84dabb0b54774a99cb96d0a494c81
np11.dev <niki_ponce@hotmail.com>
2024-02-28 17:15:46 +0100

    parse parameters

--- src/app/dates.py
+++ src/app/dates.py
@@ -2,7 +2,9 @@ import dateparser
 
 def transform_date(date, dateFormat: str = "%Y-%m-%d"):
     try:
-        parsed_date = dateparser.parse(date)
+        settings = {'STRICT_PARSING': True} # Fechas completas
+        parsed_date = dateparser.parse(fecha, settings=settings, languages=['es'])
+        # parsed_date = dateparser.parse(date)
         return parsed_date.strftime(dateFormat)
     except (ValueError, AttributeError):
         return None
