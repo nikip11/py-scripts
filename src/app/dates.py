597594c181139c05524d7a907170b3c9776ac65e
Niki Joel <niki_ponce@hotmail.com>
2024-02-28 22:28:34 +0100

    test dates

--- src/app/dates.py
+++ src/app/dates.py
@@ -3,7 +3,7 @@ import dateparser
 def transform_date(date, dateFormat: str = "%Y-%m-%d"):
     try:
         settings = {'STRICT_PARSING': True}
-        parsed_date = dateparser.parse(date, settings=settings, languages=['es'])
+        parsed_date = dateparser.parse(date, settings=settings)
         return parsed_date.strftime(dateFormat)
     except (ValueError, AttributeError):
         return None
--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,11 @@
-from typing import Union
+import warnings
 
+from typing import Union
 from fastapi import FastAPI, Query
 
 from .dates import transform_date
 from .config import title
-import warnings
+from .testdates import dates
 
 app = FastAPI(title=title)
 
@@ -25,3 +26,17 @@ def format_date(date: str, dateFormat: str = '%Y-%m-%d'):
     """
     final_date = transform_date(date, dateFormat)
     return {"code": "200", "dateFormatted": final_date, "dateFormat": dateFormat}
+
+
+@app.get("/test-dates", tags=['dates'])
+def test_dates():
+    # get date on file
+    response = []
+    for d in dates:
+        final_date = transform_date(d)
+        response.append({
+            "origin": d,
+            "final": final_date
+        })
+    # order by None
+    return response
